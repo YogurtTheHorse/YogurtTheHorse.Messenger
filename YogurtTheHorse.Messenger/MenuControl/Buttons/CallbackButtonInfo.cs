0565ea39bcf2861a19f6183cdbe1b4825cc0a827
yegorf1 <yegorf1@gmail.com>
2018-03-23 21:21:03 +0300

    Added callback button

---
+++ YogurtTheHorse.Messenger/MenuControl/Buttons/CallbackButtonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YogurtTheHorse.Messenger.MenuControl.Buttons {
+	public sealed class CallbackButtonInfo<TUserData> : UsualButtonInfo where TUserData : class, IUserData {
+		private Action<object, ButtonActionEventArgs<TUserData>> _callback;
+		public override EButtonType ButtonType { get; }
+
+		public CallbackButtonInfo(string text, Action<object, ButtonActionEventArgs<TUserData>> callback) : base(text) {
+			_callback = callback;
+		}
+
+		public override void Action<T>(object sender, ButtonActionEventArgs<T> e) {
+			_callback(sender, e as ButtonActionEventArgs<TUserData>);
+		}
+	}
+}
