8a8a5a0caba78ca5368e4e3038d7771ed9911781
yegorf1 <yegorf1@gmail.com>
2018-03-23 21:21:40 +0300

    Added variable menu

---
+++ YogurtTheHorse.Messenger/MenuControl/VariableMenuCreator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace YogurtTheHorse.Messenger.MenuControl {
+	public static class VariableMenuCreator {
+		public static VariableUserMenu<TUserData, TVariable> CreateVariableMenu<TUserData, TVariable>(
+				MenuController<TUserData> controller,
+				Expression<Func<TUserData, TVariable>> memberExpression,
+				Func<string, TVariable> parse) where TUserData : class, IUserData {
+			var menu = new VariableUserMenu<TUserData, TVariable>(controller, memberExpression, parse);
+			try {
+				controller.RegisterMenuInstance(menu);
+			} catch (ArgumentException e) {
+				throw new ArgumentException("Variable menu for that member alread registered", e);
+			}
+			return menu;
+		}
+
+		public static VariableUserMenu<TUserData, string> CreateVariableMenu<TUserData, TVariable>(
+				MenuController<TUserData> controller,
+				Expression<Func<TUserData, string>> memberExpression) where TUserData : class, IUserData {
+			return CreateVariableMenu(controller, memberExpression, s => s);
+		}
+
+		public static VariableUserMenu<TUserData, TVariable> CreateVariableMenu<TUserData, TVariable>(
+				MenuController<TUserData> controller,
+				Expression<Func<TUserData, TVariable>>
+				memberExpression) where TUserData : class, IUserData {
+			TypeConverter converter = TypeDescriptor.GetConverter(typeof(TVariable));
+
+
+			if (!(converter is null) && converter.CanConvertFrom(typeof(string))) {
+				return CreateVariableMenu(controller, memberExpression, s => (TVariable)converter.ConvertFromString(s));
+			} else {
+				throw new NotSupportedException($"Can't convert {typeof(TVariable)} from string with TypeConverter. You should write own parse function");
+			}
+		}
+	}
+}
---
+++ YogurtTheHorse.Messenger/MenuControl/VariableUserMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using YogurtTheHorse.Messenger.MenuControl.Buttons;
+
+namespace YogurtTheHorse.Messenger.MenuControl {
+	public class VariableUserMenu<TUserData, TVariable> : SimpleUserMenu<TUserData> where TUserData : class, IUserData {
+		protected bool IsProperty { get; }
+		protected MemberExpression MemberExpression { get; }
+		protected Func<string, TVariable> Parse { get; }
+
+		protected override string StartMessage { get; } = "Please specify variable:";
+		public override string MenuName => MemberExpression.Member.Name + "Menu";
+
+
+		internal VariableUserMenu(MenuController<TUserData> menuController, Expression<Func<TUserData, TVariable>> memberExpression, Func<string, TVariable> parse)
+		: base(menuController) {
+			MemberExpression = memberExpression.Body as MemberExpression;
+			if (memberExpression is null) {
+				throw new ArgumentException($"Expression '{memberExpression}' refers to a method, not a property.");
+			}
+
+			IsProperty = MemberExpression.Member is PropertyInfo;
+			Layout = new ButtonInfo[0][];
+		}
+
+		public override void OnMessage(Message message, TUserData userData) {
+			TVariable parsed = Parse(message.Text);
+			if (IsProperty) {
+				(MemberExpression.Member as PropertyInfo).SetValue(userData, parsed);
+			} else {
+				(MemberExpression.Member as FieldInfo).SetValue(userData, parsed);
+			}
+		}
+	}
+}
