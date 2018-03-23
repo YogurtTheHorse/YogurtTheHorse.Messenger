670b7b6bab1eadd3f7d7955c24186fe91d612d48
yegorf1 <yegorf1@gmail.com>
2018-03-23 19:30:31 +0300

    Changed properties of fields in SimpleUserMenu

--- YogurtTheHorse.Messenger/MenuControl/SimpleUserMenu.cs
+++ YogurtTheHorse.Messenger/MenuControl/SimpleUserMenu.cs
@@ -1,25 +1,27 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using YogurtTheHorse.Messenger.MenuControl.Buttons;
 
 namespace YogurtTheHorse.Messenger.MenuControl {
-    public abstract class SimpleUserMenu<TUserData> : IUserMenu where TUserData : class, IUserData {
-        protected MenuController<TUserData> _menuController;
+	public abstract class SimpleUserMenu<TUserData> : IUserMenu where TUserData : class, IUserData {
+		protected MenuController<TUserData> _menuController;
+		protected virtual ButtonLayout Layout { get; set; }
 
-        protected abstract ButtonLayout Layout { get; }
-        protected abstract string StartMessage { get; }
+		protected abstract string StartMessage { get; }
 
-        public abstract string MenuName { get; }
+		public virtual string MenuName => GetType().Name;
 
-        public SimpleUserMenu(MenuController<TUserData> menuController) {
-            _menuController = menuController;
-        }
+		public SimpleUserMenu(MenuController<TUserData> menuController) {
+			_menuController = menuController;
+		}
 
-        public virtual void Open(User user, IUserData userData, object sender) {
-            user.SendMessage(StartMessage, Layout);
-        }
+		public virtual void Open(User user, IUserData userData, object sender) {
+			user.SendMessage(StartMessage, Layout);
+		}
 
-        public void OnMessage(Message message, IUserData userData) {
-            OnMessage(message, (TUserData)userData);
+		public void OnMessage(Message message, IUserData userData) {
+			OnMessage(message, (TUserData)userData);
 		}
 
 		public void OnUnusualMessage(Message message, IUserData userData) {
@@ -30,9 +32,9 @@ namespace YogurtTheHorse.Messenger.MenuControl {
 		}
 
 		public virtual void OnMessage(Message message, TUserData userData) {
-            if (Layout.LayoutType != EButtonType.Usual) { return; }
+			if (Layout.LayoutType != EButtonType.Usual) { return; }
 
-            ButtonInfo bi = Layout.GetAllButtons().FirstOrDefault(b => b.Text == message.Text);
+			ButtonInfo bi = Layout.GetAllButtons().FirstOrDefault(b => b.Text == message.Text);
 
 			if (bi is null) {
 				OnUnusualMessage(message, userData);
@@ -45,8 +47,13 @@ namespace YogurtTheHorse.Messenger.MenuControl {
 					UserData = userData
 				});
 			}
-        }
+		}
 
-        public virtual void Close(User user, IUserData userData, object sender) { }
-    }
+		public virtual void Close(User user, IUserData userData, object sender) { }
+
+		public static NavigationButtonInfo Navigate(string text) {
+			string className = new StackTrace(false).GetFrame(1).GetMethod().DeclaringType.Name.ToString();
+			return new NavigationButtonInfo(text, className);
+		}
+	}
 }
