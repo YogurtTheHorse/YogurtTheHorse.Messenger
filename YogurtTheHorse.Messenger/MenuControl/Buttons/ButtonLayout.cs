483300772afdef4bcaad587ea7a7f43ce06e3bca
yegorf1 <yegorf1@gmail.com>
2018-03-23 18:46:09 +0300

    Added default values class for Button layout

--- YogurtTheHorse.Messenger/MenuControl/Buttons/ButtonLayout.cs
+++ YogurtTheHorse.Messenger/MenuControl/Buttons/ButtonLayout.cs
@@ -3,50 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace YogurtTheHorse.Messenger.MenuControl.Buttons {
-    public class ButtonLayout {
-        protected List<List<ButtonInfo>> Buttons;
+	public class ButtonLayout {
+		public static DefaultButtonLayoutValues DefaultButtonLayoutValues { get; set; }
 
-        public virtual EButtonType LayoutType { get; }
-        public virtual bool ResizeKeyboard { get; }
-        public virtual bool OneTimeKeyboard { get; }
+		protected List<List<ButtonInfo>> Buttons;
+
+		public virtual EButtonType LayoutType { get; }
+		public virtual bool ResizeKeyboard { get; }
+		public virtual bool OneTimeKeyboard { get; }
 
 		#region Constructors
-		public ButtonLayout() : this(false, false) { }
+		public ButtonLayout() : this(DefaultButtonLayoutValues.ResizeKeyboard, DefaultButtonLayoutValues.OneTimeKeyboard) { }
 
-        public ButtonLayout(bool resizeKeyboard, bool oneTimeKeyboard) :
-            this(resizeKeyboard, oneTimeKeyboard, EButtonType.Other) { }
+		public ButtonLayout(bool resizeKeyboard, bool oneTimeKeyboard)
+		: this(resizeKeyboard, oneTimeKeyboard, EButtonType.Other) { }
 
-        public ButtonLayout(bool resizeKeyboard, bool oneTimeKeyboard, EButtonType layoutType) {
-            ResizeKeyboard = resizeKeyboard;
-            OneTimeKeyboard = oneTimeKeyboard;
+		public ButtonLayout(bool resizeKeyboard, bool oneTimeKeyboard, EButtonType layoutType) {
+			ResizeKeyboard = resizeKeyboard;
+			OneTimeKeyboard = oneTimeKeyboard;
 
-            Buttons = new List<List<ButtonInfo>>();
+			Buttons = new List<List<ButtonInfo>>();
 			LayoutType = layoutType;
-        }
+		}
 
-        public ButtonLayout(ICollection<ButtonInfo> buttons, bool isVertical, bool resizeKeyboard, bool oneTimeKeyboard) :
-            this(resizeKeyboard, oneTimeKeyboard) {
+		public ButtonLayout(ICollection<ButtonInfo> buttons, bool isVertical, bool resizeKeyboard, bool oneTimeKeyboard)
+		: this(resizeKeyboard, oneTimeKeyboard) {
 
-            if (isVertical) {
-                LayoutType = buttons?.GetEnumerator().Current?.ButtonType ?? EButtonType.Other;
-                Buttons.Add(new List<ButtonInfo>(buttons));
-            } else {
-                foreach (var b in buttons) {
-                    LayoutType = b.ButtonType;
-                    Buttons.Add(new[] { b }.ToList());
-                }
-            }
+			if (isVertical) {
+				LayoutType = buttons?.GetEnumerator().Current?.ButtonType ?? EButtonType.Other;
+				Buttons.Add(new List<ButtonInfo>(buttons));
+			} else {
+				foreach (var b in buttons) {
+					LayoutType = b.ButtonType;
+					Buttons.Add(new[] { b }.ToList());
+				}
+			}
 
-            CheckButtonsType();
-        }
+			CheckButtonsType();
+		}
 
-        public ButtonLayout(ICollection<ICollection<ButtonInfo>> buttons) : this(buttons, false, false) { }
+		public ButtonLayout(ICollection<ICollection<ButtonInfo>> buttons)
+		: this(buttons, DefaultButtonLayoutValues.ResizeKeyboard, DefaultButtonLayoutValues.OneTimeKeyboard) { }
 
-        public ButtonLayout(ICollection<ICollection<ButtonInfo>> buttons, bool resizeKeyboard, bool oneTimeKeyboard) : this(resizeKeyboard, oneTimeKeyboard) {
-            Buttons = new List<List<ButtonInfo>>();
+		public ButtonLayout(ICollection<ICollection<ButtonInfo>> buttons, bool resizeKeyboard, bool oneTimeKeyboard) : this(resizeKeyboard, oneTimeKeyboard) {
+			Buttons = new List<List<ButtonInfo>>();
 			Buttons.AddRange(buttons.Select(row => new List<ButtonInfo>(row)));
 
-            CheckButtonsType();
+			CheckButtonsType();
+		}
+
+
+
+		public static implicit operator ButtonLayout(ButtonInfo[] btns) {
+			return new ButtonLayout(btns, true, false, false);
+		}
+
+		public static implicit operator ButtonLayout(ButtonInfo[][] btns) {
+			return new ButtonLayout(btns, false, false);
 		}
 		#endregion
 
@@ -54,24 +67,24 @@ namespace YogurtTheHorse.Messenger.MenuControl.Buttons {
 
 		#region Enumerators
 		public IEnumerable<ButtonInfo> GetAllButtons() {
-            foreach (var lb in Buttons) {
-                foreach (var bi in lb) {
-                    yield return bi;
-                }
-            }
-        }
+			foreach (var lb in Buttons) {
+				foreach (var bi in lb) {
+					yield return bi;
+				}
+			}
+		}
 
-        public IEnumerable<ButtonInfo> GetAllUsualButtons() {
-            foreach (var btn in GetAllButtons()) {
+		public IEnumerable<ButtonInfo> GetAllUsualButtons() {
+			foreach (var btn in GetAllButtons()) {
 				if (btn.ButtonType == EButtonType.Usual) {
 					yield return btn;
 				}
-            }
-        }
+			}
+		}
 
 		public ButtonInfo[][] GetButtons() {
 			ButtonInfo[][] buttons = new ButtonInfo[Buttons.Count][];
-			
+
 			for (int i = 0; i < Buttons.Count; i++) {
 				buttons[i] = new ButtonInfo[Buttons[i].Count];
 
---
+++ YogurtTheHorse.Messenger/MenuControl/DefaultClasses.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YogurtTheHorse.Messenger.MenuControl {
+	public struct DefaultButtonLayoutValues {
+		public bool ResizeKeyboard { get; set; }
+		public bool OneTimeKeyboard { get; set; }
+	}
+}
