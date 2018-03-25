af9a90525711795ea9921fd2ef16be59b7117522
yegorf1 <yegorf1@gmail.com>
2018-03-25 19:36:01 +0300

    Moved token configurration to config file

--- Commentgram.Bot/Program.cs
+++ Commentgram.Bot/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 using Commentgram.Bot.Menus;
 using Commentgram.Bot.Menus.AccountMenus;
+
 using YogurtTheHorse.Messenger.Database.Mongo;
 using YogurtTheHorse.Messenger.MenuControl;
 using YogurtTheHorse.Messenger.Telegram;
+using System.IO;
 
 namespace Commentgram.Bot {
-    public class Program {
-        public static void Main(string[] args) {
+	public class Program {
+		public static void Main(string[] args) {
+			var tokensConfigurationBuilder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("tokens.json");
+
+			IConfiguration tokensConfigutaion = tokensConfigurationBuilder.Build();
+
 			var mongoDriver = new MongoDriver("commentgram");
 			mongoDriver.RegisterUserDataClass<CommentgramUserData>();
-            var telegramMessenger = new TelegramMessenger("191656458:AAFHuRzACeNHnNf23ATEkuwJF4fuU7mFjZQ", mongoDriver);
+			var telegramMessenger = new TelegramMessenger(tokensConfigutaion["telegram_token"], mongoDriver);
 
-            MenuController menuController = new MenuController(telegramMessenger, (s) => new CommentgramUserData(s));
+			MenuController menuController = new MenuController(telegramMessenger, (s) => new CommentgramUserData(s));
 			menuController.RegisterMenuInstance(new MainMenu(menuController));
 			menuController.RegisterMenuInstance(new AccountMenu(menuController));
 			menuController.RegisterMenuInstance(new WalletMenu(menuController));
@@ -22,6 +31,6 @@ namespace Commentgram.Bot {
 			telegramMessenger.Launch();
 			Console.WriteLine("Now listen to music");
 			Console.ReadLine();
-        }
-    }
+		}
+	}
 }
