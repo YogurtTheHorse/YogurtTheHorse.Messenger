adc2798a965aa9ef27f2893d6783a0ede49c4f3f
yegorf1 <yegorf1@gmail.com>
2018-03-28 16:42:04 +0300

    Added builders class

---
+++ YogurtTheHorse.Messenger/MenuControl/MenuBuilders/Builders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YogurtTheHorse.Messenger.MenuControl.MenuBuilders {
+	public static class Builders {
+		public static MenuBuilder MenuBuilder => new MenuBuilder();
+		public static MenuControllerBuilder MenuControllerBuilder => new MenuControllerBuilder();
+		public static LayoutBuilder LayoutBuilder => new LayoutBuilder();
+		public static ButtonInfoBuilder ButtonInfoBuilder => new ButtonInfoBuilder();
+
+		public static VariableMenuBuilder<T> GetVariableMenuBuilder<T>() => new VariableMenuBuilder<T>();
+		public static VariableMenuBuilder<T, U> GetVariableMenuBuilder<T, U>() where U : UserData => new VariableMenuBuilder<T, U>();
+	}
+}
