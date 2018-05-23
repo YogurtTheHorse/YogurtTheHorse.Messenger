56ac4adc38178e0347f3f98a4e49bea3ef2a423c
yegorf1 <yegorf1@gmail.com>
2018-05-23 20:43:37 +0300

    Preparing for some kind of release

--- Commentgram.Bot/Core/Money/IWallet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Commentgram.Bot.Core.Money {
-	public interface IWallet {
-		decimal Amount { get; }
-	}
-}
--- Commentgram.Bot/DataTypes/Comment.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Commentgram.Bot.DataTypes {
-	public class Comment {
-		public string AuthorID { get; set; }
-		public string LinkToPost { get; set; }
-		public string Text { get; set; }
-	}
-}
